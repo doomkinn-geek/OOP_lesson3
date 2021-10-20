c9f44a5a5223bbd6f341bed09d875b6a646f2dc1
doomkinn@yandex.ru <doomkinn@yandex.ru>
2021-10-20 16:49:30 +0300

    task 2 done

---
+++ task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.Write("Введите строку: ");
+            str = Console.ReadLine();
+            Console.WriteLine($"Реверс введнной строки: {ReverseString(str)}");
+        }
+
+        static string ReverseString(string input)
+        {
+            string res = "";
+            for(int i = input.Length-1;i>=0;i--)
+            {
+                res += input[i];
+            }
+            return res;
+        }
+    }
+}
