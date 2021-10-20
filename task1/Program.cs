f58aa1c93fb7daf5b5aa66c6feb27c94d26e1f05
doomkinn@yandex.ru <doomkinn@yandex.ru>
2021-10-20 16:44:29 +0300

    task 1 testing done

--- task1/Program.cs
+++ task1/Program.cs
@@ -6,7 +6,23 @@ namespace task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount account = new BankAccount(0, AccountType.Deposit);
+            BankAccount account1 = new BankAccount(90000);
+            Console.WriteLine(account);
+            Console.WriteLine("Кладем на счет 50000");
+            account.IncreaseBalance(50000);
+            Console.WriteLine(account);
+            Console.WriteLine("Снимаем 10000");
+            account.GetMoney(10000);
+            Console.WriteLine(account);
+            Console.WriteLine("Снимаем 50000");
+            account.GetMoney(50000);
+            Console.WriteLine(account);
+            Console.WriteLine(account1);
+            Console.WriteLine("Переводим 40000 с account1 на account");
+            account.TransferMoney(account1, 40000);
+            Console.WriteLine(account);
+            Console.WriteLine(account1);
         }
     }
 }
